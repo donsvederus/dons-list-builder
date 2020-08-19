658bbd7cdd301a6c9f925babd46e462393583c9d
donsvederus <58320530+donsvederus@users.noreply.github.com>
2020-08-19 01:31:16 -0600

    Initial commit

---
+++ don-list-builder.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+Plugin Name: Dons List Builder
+Plugin URI: https://myseoplugin/plugins/dons-list-builder
+Description: The is an email list building plugin for WordPress.  Tutorial by Joel Funk
+Version: 0.1.12
+Author: Don Svederus
+Author URI: https://svederus.com
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: dons-list-builder
+*/
+
